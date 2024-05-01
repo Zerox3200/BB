158167ad9f511ad3ec0d68ae246354c3556c5473
Zerox3200 <ziad.ahmed01020338066@gmail.com>
2024-05-02 00:23:12 +0300

    Searching

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -13,12 +13,12 @@ function Search() {
     return (
         <div className="Intro py-3 px-4 row w-100 column-gap-3 align-items-center">
             <MdOutlineMenu onClick={HandleMobileNav} />
-            <div className="Intro_Search">
-                <input type="text" className="form-control" id='search' />
+            <form className="Intro_Search">
                 <label htmlFor="search">
                     <IoSearchOutline />
                 </label>
-            </div>
+                <input type="text" className="form-control" id='search' />
+            </form>
 
             <Languages />
         </div>
