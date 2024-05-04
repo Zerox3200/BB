7999702d90a659553b67bd1314b293fa14222147
Zerox3200 <ziad.ahmed01020338066@gmail.com>
2024-05-04 21:07:29 +0300

    Updates

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,26 +1,34 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { IoSearchOutline } from 'react-icons/io5';
 import { MdOutlineMenu } from 'react-icons/md';
 import { NavContext } from '../../Context/NavContext';
 import Languages from '../Language/Language';
 
 import './Search.scss';
+import { useLocation } from 'react-router-dom';
 
 function Search() {
     const { HandleMobileNav } = useContext(NavContext);
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+
+    }, [pathname])
 
 
     return (
-        <div className="Intro py-3 px-4 row w-100 column-gap-3 align-items-center">
+        <div className={pathname === "/Policy" ? "Intro py-3 row w-100 column-gap-3 align-items-center" : "Intro py-3 px-4 row w-100 column-gap-3 align-items-center"}>
             <MdOutlineMenu onClick={HandleMobileNav} />
-            <form className="Intro_Search">
-                <label htmlFor="search">
-                    <IoSearchOutline />
-                </label>
-                <input type="text" className="form-control" id='search' placeholder='Search' />
-            </form>
-
-            <Languages />
+            {pathname === "/Policy" ? null : <>
+                <form className="Intro_Search">
+                    <label htmlFor="search">
+                        <IoSearchOutline />
+                    </label>
+                    <input type="text" className="form-control" id='search' placeholder='Search' />
+                </form>
+                <Languages />
+            </>
+            }
         </div>
     );
 };
